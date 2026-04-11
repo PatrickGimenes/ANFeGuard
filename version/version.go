7a9f5e831e97613d311ec6dbf3189c242f4d56cf
Patrick Gimenes <p.gimenes@outlook.com>
2026-04-11 17:38:49 -0300

    fix(monitoring): prevent duplicate email alerts on high resource usage

--- version/version.go
+++ version/version.go
@@ -1,3 +1,6 @@
 package version
 
-const Version = "1.0.0"
+const Version = "1.0.1"
+
+// 1.0.1 - Adicionado trataviva para evitar spam de e-mails quando tiver alto consumo de recursos do servidor - 11/04/2026
+// 1.0.0 - Primeira versão oficial do ANFeGuard - 15/03/2026
